f4c0f5cfe6d9971332f2fbaf0fb4724c255c49a5
Rafaín Rodríguez San Millán <38697752+rafain@users.noreply.github.com>
2018-10-03 11:55:43 -0500

    FPASE almost done

---
+++ client.py
@@ -0,0 +1,59 @@
+import socket
+import threading as THR
+
+def receive():
+    while True:
+        try:
+            msg = client_socket.recv(1024)
+            print(msg)
+        except OSEroor:   #Trouble
+            break
+
+def send(event=None):
+    while True:
+        msg = str(raw_input("3"))
+        client_socket.send(msg)
+        if msg == "quit":
+            client_socket.close()
+
+def encrypt_stream(data, key):
+    encrypted = ""
+    for char in data:
+        encrypted += chr(ord(char)^key)
+    return encrypted
+
+def decrypt_stream(data, key):
+    decrypted = ""
+    for char in data:
+        decrypted += chr(ord(char)^key)
+    return decrypted
+
+def init():
+    i=0
+    msg = ""
+    while i<2:
+        try:
+            msg = client_socket.recv(1024)
+            print(msg)
+            i += 1
+        except OSEroor:   #Trouble
+            break
+    return msg.split(" ")
+
+
+HOST = 'localhost'
+port = 8291
+
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST, port))
+
+
+key = int(raw_input("Type in your key: "))
+
+receive_thread = THR.Thread(name = "r", target=receive)
+receive_thread.start()
+
+send_thread = THR.Thread(name = "s",target=send)
+send_thread.start()
+
--- server.py
+++ server.py
@@ -1,26 +1,49 @@
-# https://medium.com/swlh/lets-write-a-chat-app-in-python-f6783a9ac170
-#Rafaín Rodríguez San Millán A01372976
+#https://medium.com/swlh/lets-write-a-chat-app-in-python-f6783a9ac170
 
 import socket
-import threading
+import threading as THR
 
 clients = {}
 addrs = {}
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('HostServer', 8291))
+server.bind(('localhost', 8291))
 
 def accept_conections():
     while True:
         client, client_addr = server.accept()
         print("%s:%s has connected." % client_addr)
-        client.send(bytes("Welcome to this ultra super secret chat" + "Type your codename and press enter: ", "utf8"))
+        client.send("Welcome to this ultra super secret chat. Type your codename and press enter: ")
         addrs[client] = client_addr
-        Thread(target=handle_client, args=(client,)).start()
+        THR.Thread(target=handle_client, args=(client,)).start()
 
 def handle_client(client):
-    name = client.recv(1024).decode("utf8")
-    welcome = 'Welcome %s! If you want to quito, type "quit" to exit.' % name
-    client.send(bytes(welcome, "utf8"))
+    name = client.recv(1024)
+    welcome = 'Welcome %s! If you want to quit, type "quit" to exit.' % name
+    client.send(welcome)
+    clients[client] = name
+    while True:
+        msg = client.recv(1024)
+        if msg !=  "quit":
+            broadcast(msg, name+ ": ")
+        else:
+            client.send("quitting")
+            client.close()
+            del clients[client]
+            broadcast("%s has left the chat." % name)
+            break
 
-server.listen(2)
+def broadcast(msg, prefix=""):
+    for sock in clients:
+        sock.send(prefix + msg)
+
+
+def main():
+    server.listen(4)
+    print("Waiting for connection...")
+    ACCEPT_THREAD = THR.Thread(target=accept_conections)
+    ACCEPT_THREAD.start()
+    ACCEPT_THREAD.join()
+    server.close()
+
+main()
